df3ff77090ee708e6f256a38c040edb27a8c712b
Alessandro Cavaliere <alexcavaliere517@gmail.com>
2023-04-07 16:25:50 +0200

    first table creation

--- wealth-app/src/components/Form.js
+++ wealth-app/src/components/Form.js
@@ -1,7 +1,22 @@
 import { useState } from "react";
+import Modal from "react-modal"
 import DatePicker from "react-datepicker";
 import Select from 'react-select'
 import "react-datepicker/dist/react-datepicker.css"
+
+const data = JSON.parse(localStorage.getItem('employees'))
+console.log(data)
+
+const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    }
+}
 
 const options = [
     { value: 'Marketing', label: 'Marketing' },
@@ -10,281 +25,76 @@ const options = [
     { value: 'Legal', label: 'Legal' }
 ]
 
+
 function Form(){
     const [startDate, setStartDate] = useState(new Date())
+    const saveEmployee = (e) => {
+        e.preventDefault()
+        const employees = JSON.parse(localStorage.getItem('employees')) || []
+        const employee = {
+            firstName: e.target.firstName.value,
+            lastName: e.target.lastName.value,
+            dateOfBirth: e.target.dateOfBirth.value,
+            startDate: e.target.startDate.value,
+            street: e.target.street.value,
+            city: e.target.city.value,
+            zipCode: e.target.zipCode.value
+        } 
+        employees.push(employee)
+        localStorage.setItem('employees', JSON.stringify(employees))
+        openModal()
+    }
+    const [modalIsOpen, setIsOpen] = useState(false);
+  
+    function openModal() {
+      setIsOpen(true);
+    }
+    function closeModal() {
+        setIsOpen(false)
+    }
     return(
-        <form id="create-employee">
+        <form onSubmit={saveEmployee} id="create-employee">
             <label htmlFor="first-name">First Name</label>
-            <input type="text" id="first-name" />
+            <input type="text" id="first-name" name="firstName" required/>
 
             <label htmlFor="last-name">Last Name</label>
-            <input type="text" id="last-name" />
+            <input type="text" id="last-name" name="lastName" required/>
 
             <label htmlFor="date-of-birth">Date of Birth</label>
-            <DatePicker id="date-of-birth" selected={startDate} onChange={(date) => setStartDate(date)} />
+            <DatePicker id="date-of-birth" name="dateOfBirth" selected={startDate} onChange={(date) => setStartDate(date)} />
 
             <label htmlFor="start-date">Start Date</label>
-            <DatePicker id="start-date" selected={startDate} onChange={(date) => setStartDate(date)} />
+            <DatePicker id="start-date" name="startDate" selected={startDate} onChange={(date) => setStartDate(date)} />
 
             <fieldset className="address">
                 <legend>Address</legend>
 
                 <label htmlFor="street">Street</label>
-                <input id="street" type="text" />
+                <input id="street" type="text" name="street"/>
 
                 <label htmlFor="city">City</label>
-                <input id="city" type="text" />
+                <input id="city" type="text" name="city"/>
 
                 <label htmlFor="state">State</label>
-                <select name="state" id="state"></select>
+
 
                 <label htmlFor="zip-code">Zip Code</label>
-                <input id="zip-code" type="number" />
+                <input id="zip-code" type="number" name="zipCode" />
             </fieldset>
 
             <label htmlFor="department">Department</label>
             <Select options={options} />
+            <button id="submit">save</button>
+            <Modal
+                isOpen={modalIsOpen}
+                ariaHideApp={false}
+                onRequestClose={closeModal}
+                style={customStyles}>
+                <button className="close" onClick={closeModal}>X</button>
+                <h2 id="confirmation" className="modal">Employee Created!</h2>
+            </Modal>
         </form>
     )
 }
 
 export default Form
-
-const states = [
-    {
-        value: "Alabama",
-        label: "AL"
-    },
-    {
-        value: "Alaska",
-        label: "AK"
-    },
-    {
-        value: "American Samoa",
-        label: "AS"
-    },
-    {
-        value: "Arizona",
-        label: "AZ"
-    },
-    {
-        value: "Arkansas",
-        label: "AR"
-    },
-    {
-        value: "California",
-        label: "CA"
-    },
-    {
-        value: "Colorado",
-        label: "CO"
-    },
-    {
-        value: "Connecticut",
-        label: "CT"
-    },
-    {
-        value: "Delaware",
-        label: "DE"
-    },
-    {
-        value: "District Of Columbia",
-        label: "DC"
-    },
-    {
-        value: "Federated States Of Micronesia",
-        label: "FM"
-    },
-    {
-        value: "Florida",
-        label: "FL"
-    },
-    {
-        value: "Georgia",
-        label: "GA"
-    },
-    {
-        value: "Guam",
-        label: "GU"
-    },
-    {
-        value: "Hawaii",
-        label: "HI"
-    },
-    {
-        value: "Idaho",
-        label: "ID"
-    },
-    {
-        value: "Illinois",
-        label: "IL"
-    },
-    {
-        value: "Indiana",
-        label: "IN"
-    },
-    {
-        value: "Iowa",
-        label: "IA"
-    },
-    {
-        value: "Kansas",
-        label: "KS"
-    },
-    {
-        value: "Kentucky",
-        label: "KY"
-    },
-    {
-        value: "Louisiana",
-        label: "LA"
-    },
-    {
-        value: "Maine",
-        label: "ME"
-    },
-    {
-        value: "Marshall Islands",
-        label: "MH"
-    },
-    {
-        value: "Maryland",
-        label: "MD"
-    },
-    {
-        value: "Massachusetts",
-        label: "MA"
-    },
-    {
-        value: "Michigan",
-        label: "MI"
-    },
-    {
-        value: "Minnesota",
-        label: "MN"
-    },
-    {
-        value: "Mississippi",
-        label: "MS"
-    },
-    {
-        value: "Missouri",
-        label: "MO"
-    },
-    {
-        value: "Montana",
-        label: "MT"
-    },
-    {
-        value: "Nebraska",
-        label: "NE"
-    },
-    {
-        value: "Nevada",
-        label: "NV"
-    },
-    {
-        value: "New Hampshire",
-        label: "NH"
-    },
-    {
-        value: "New Jersey",
-        label: "NJ"
-    },
-    {
-        value: "New Mexico",
-        label: "NM"
-    },
-    {
-        value: "New York",
-        label: "NY"
-    },
-    {
-        value: "North Carolina",
-        label: "NC"
-    },
-    {
-        value: "North Dakota",
-        label: "ND"
-    },
-    {
-        value: "Northern Mariana Islands",
-        label: "MP"
-    },
-    {
-        value: "Ohio",
-        label: "OH"
-    },
-    {
-        value: "Oklahoma",
-        label: "OK"
-    },
-    {
-        value: "Oregon",
-        label: "OR"
-    },
-    {
-        value: "Palau",
-        label: "PW"
-    },
-    {
-        value: "Pennsylvania",
-        label: "PA"
-    },
-    {
-        value: "Puerto Rico",
-        label: "PR"
-    },
-    {
-        value: "Rhode Island",
-        label: "RI"
-    },
-    {
-        value: "South Carolina",
-        label: "SC"
-    },
-    {
-        value: "South Dakota",
-        label: "SD"
-    },
-    {
-        value: "Tennessee",
-        label: "TN"
-    },
-    {
-        value: "Texas",
-        label: "TX"
-    },
-    {
-        value: "Utah",
-        label: "UT"
-    },
-    {
-        value: "Vermont",
-        label: "VT"
-    },
-    {
-        value: "Virgin Islands",
-        label: "VI"
-    },
-    {
-        value: "Virginia",
-        label: "VA"
-    },
-    {
-        value: "Washington",
-        label: "WA"
-    },
-    {
-        value: "West Virginia",
-        label: "WV"
-    },
-    {
-        value: "Wisconsin",
-        label: "WI"
-    },
-    {
-        value: "Wyoming",
-        label: "WY"
-    }
-]
--- wealth-app/src/pages/EmployeeList.js
+++ wealth-app/src/pages/EmployeeList.js
@@ -1,10 +1,58 @@
 import { NavLink } from "react-router-dom"
 
+const data = JSON.parse(localStorage.getItem('employees'))
+
 function EmployeeList() {
+    console.log(data)
     return(
         <div id="employee-div" className="container">
             <h3>Current Employees</h3>
-            <table></table>
+            <div className="table-header">
+                <div className="entries">
+                Show
+                <select>
+                    <option>10</option>
+                    <option>25</option>
+                    <option>50</option>
+                    <option>100</option>
+                </select>
+                Entries
+                </div>
+                <div className="search-container">
+                    <label htmlFor="searchbar">Search:</label>
+                    <input type="text" id="searchbar" name="searchbar"/>
+                </div>
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Start Date</th>
+                        <th>Department</th>
+                        <th>Date Of Birth</th>
+                        <th>Street</th>
+                        <th>City</th>
+                        <th>State</th>
+                        <th>Zip Code</th>
+                    </tr>
+                </thead>
+                <tbody>
+                        {
+                            data.map((obj, index) => <tr>
+                                <td>{obj.firstName}</td>
+                                <td>{obj.lastName}</td>
+                                <td>{obj.startDate}</td>
+                                <td>{obj.departement}</td>
+                                <td>{obj.dateOfBirth}</td>
+                                <td>{obj.street}</td>
+                                <td>{obj.city}</td>
+                                <td>{obj.state}</td>
+                                <td>{obj.zipCode}</td>
+                            </tr>)
+                        }
+                </tbody>
+            </table>
             <NavLink to="/">Home</NavLink>
         </div>
     )
