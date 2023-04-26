1e5f1a5dfea6cdb43fbe7b0660743bf6490ba444
Alessandro Cavaliere <alexcavaliere517@gmail.com>
2023-04-26 16:25:43 +0200

    entries input control

--- wealth-app/src/components/DataTable.js
+++ wealth-app/src/components/DataTable.js
@@ -23,129 +23,40 @@ function DataTable(props) {
                         if (table.id === filterId){
                             chevronUp.classList.remove('unsorted')
                             chevronDown.classList.add('unsorted')
-                            setAscendent(false)
                         }else{
                             chevronUp.classList.add('unsorted')
-                            chevronDown.classList.remove('unsorted')
+                            chevronDown.classList.add('unsorted')
                         }
                     })    
+                    setAscendent(false) 
                 }else{
                     setSortedData([...sortedData].sort((a, b) => {return b[filterId].localeCompare(a[filterId])}))
                     tableList.forEach((table) => {
                         let chevronUp = table.children[0].children[0].children[0]
                         let chevronDown = table.children[0].children[0].children[1]
                         if (table.id === filterId){
-                            chevronUp.classList.add('unsorted')
                             chevronDown.classList.remove('unsorted')
-                            setAscendent(true)
+                            chevronUp.classList.add('unsorted')
                         }else{
+                            chevronDown.classList.add('unsorted')
                             chevronUp.classList.add('unsorted')
-                            chevronDown.classList.remove('unsorted')
                         }
-                    })    
+                    })   
+                    setAscendent(true) 
                 }
-                break;
-                default :
-                    return sortedData
-        }
-        /*
-        switch (e.target.innerText) {
-            case 'First Name': 
-            if(ascendent){
-                setSortedData([...sortedData].sort((a, b) => {return a.firstName.localeCompare(b.firstName)}))
-                setAscendent(false)
-            }else{
-                setSortedData([...sortedData].sort((a, b) => {return b.firstName.localeCompare(a.firstName)}))
-                setAscendent(true)
-            }
             break;
-            case 'Last Name': 
-            if(ascendent){
-                setSortedData([...sortedData].sort((a, b) => {return a.lastName.localeCompare(b.lastName)}))
-                setAscendent(false)
-            }else{
-                setSortedData([...sortedData].sort((a, b) => {return b.lastName.localeCompare(a.lastName)}))
-                setAscendent(true)
-            }
-            break;
-            case 'Date Of Birth': 
-            if(ascendent){
-                setSortedData([...sortedData].sort((a, b) => {return new Date(a.dateOfBirth) - new Date(b.dateOfBirth)}))
-                setAscendent(false)
-            }else{
-                setSortedData([...sortedData].sort((a, b) => {return new Date(b.dateOfBirth) - new Date(a.dateOfBirth)}))
-                setAscendent(true)    
-            }
-            break;
-            case 'Start Date': 
-            if(ascendent){
-                setSortedData([...sortedData].sort((a, b) => {return new Date(a.startDate) - new Date(b.startDate)}))
-                setAscendent(false)
-            }else{
-                setSortedData([...sortedData].sort((a, b) => {return new Date(b.startDate) - new Date(a.startDate)}))
-                setAscendent(true)
-            }
-            break;
-            case 'Department': 
-            if(ascendent){
-                setSortedData([...sortedData].sort((a, b) => {return a.department.localeCompare(b.department)}))
-                setAscendent(false)
-            }else{
-                setSortedData([...sortedData].sort((a, b) => {return b.department.localeCompare(a.department)}))
-                setAscendent(true)
-            }
-            break;
-            case 'Street':
-            if(ascendent){
-                setSortedData([...sortedData].sort((a, b) => {return a.street.localeCompare(b.street)}))
-                setAscendent(false)
-            }else{
-                setSortedData([...sortedData].sort((a, b) => {return b.street.localeCompare(a.street)}))
-                setAscendent(true)
-            }
-            break;
-            case 'State': 
-            if(ascendent){
-                setSortedData([...sortedData].sort((a, b) => {return a.state.localeCompare(b.state)}))
-                setAscendent(false)
-            }else{
-                setSortedData([...sortedData].sort((a, b) => {return b.state.localeCompare(a.state)}))
-                setAscendent(true)
-            }
-            break;
-            case 'City':
-            if(ascendent){
-                setSortedData([...sortedData].sort((a, b) => {return a.city.localeCompare(b.city)}))
-                setAscendent(false)
-            }else{
-                setSortedData([...sortedData].sort((a, b) => {return b.city.localeCompare(a.city)}))
-                setAscendent(true)
-            }
-            break;
-            case 'Zip Code': 
-            if(ascendent){
-                setSortedData([...sortedData].sort((a, b) => {return a.zipCode - b.zipCode}))
-                setAscendent(false)
-            }else{
-                setSortedData([...sortedData].sort((a, b) => {return b.zipCode - a.zipCode}))
-                setAscendent(true)
-            }
-            break;
-            default: 
+            default :
             return sortedData
         }
-        */
-        //console.log(sortedData)
     }
-    /*
     const [entry, setEntry] = useState(10)
     const [firstEntry, setFirstEntry] = useState(0)
-    const maxEntries, setMaxEntries] = useState(0)
-    */
-    const handleChange = (e) => {
-        console.log(e.target.value)
-    }
+    const [maxEntries, setMaxEntries] = useState(0)
 
+    const handleChange = (e) => {
+        setEntry(JSON.parse(e.target.value))
+    }
+    console.log(entry)
     const searchEmployee = (e) => {
         let value = e.target.value
         let result = []
@@ -204,14 +115,25 @@ function DataTable(props) {
                     !unFound ? (<tbody>
                         {
                             sortedData.map((obj, index) => <tr key={obj + index}>
-                                    {Object.values(obj).map((key, index) => <td key={key + index}>{key}</td>)}
-                            </tr>)
+                            {index < entry && Object.values(obj).map((key, index) => <td key={key + index}>{key}</td>)}
+                        </tr>)
                         }
                     </tbody>) : <tbody><tr><td colSpan='10' className="empty-list">No Data Found</td></tr></tbody>
                 }
                 <tfoot>
                     <tr>
-                        <td colSpan='7' style={{border:'none'}}>Showing {sortedData.indexOf(sortedData[1])} To {sortedData.length} Of {sortedData.length} Entries</td>
+                        <td colSpan='6'>Showing {sortedData.indexOf(sortedData[1])} To {sortedData.length > entry ? entry : sortedData.length} Of {sortedData.length} Entries</td>
+                        <td>
+                            <button>Preview</button>
+                        </td>
+                        <td className="pages">
+                            <div>
+                                1
+                            </div>
+                        </td>
+                        <td>
+                            <button>Next</button>
+                        </td>
                     </tr>
                 </tfoot>
             </table>) : <div className="empty-list">There's no Data</div>}
