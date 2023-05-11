75a76949e78d763b1ecf1b9d195677146347a3da
Alessandro Cavaliere <alexcavaliere517@gmail.com>
2023-05-11 16:48:21 +0200

    data-table install

--- wealth-app/src/pages/EmployeeList.js
+++ wealth-app/src/pages/EmployeeList.js
@@ -1,11 +1,18 @@
-import DataTable from "../components/DataTable"
+import { NavLink } from "react-router-dom"
+import { DataTable } from "my-personal-table"
 
 function EmployeeList() {
     //console.log(data)
     const data = JSON.parse(localStorage.getItem('employees'))
 
     return(
-     <DataTable data={data} />
+        <>       
+            <DataTable data={data}/>
+            <h3 className='list-title'>Current Employees</h3>
+            <footer>
+                <NavLink to="/">Home</NavLink>
+            </footer>
+        </>
     )
 }
 
